5164cfec821f3b78ef68abcb1cb1b47573cc325b
Sören Stöhrmann <soeren.stoehrmann@gmail.com>
2026-07-25 22:37:14 +0200

    test(e2e): cover workspace zoom and frame lifecycle

--- e2e/src/e2e/main.cljs
+++ e2e/src/e2e/main.cljs
@@ -1,5 +1,6 @@
 (ns e2e.main
   (:require [e2e.registry :as registry]
-            [e2e.specs.smoke]))
+            [e2e.specs.smoke]
+            [e2e.specs.workspace-mechanics]))
 
 (registry/export!)
